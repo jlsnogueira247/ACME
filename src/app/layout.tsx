e136bf2425b8cfcbb5db35a461485e7528e4ce59
Joana Nogueira <136189043+jlsnogueira247@users.noreply.github.com>
2026-02-28 15:23:14 +0000

    chore: implementar estrutura base raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type {Metadata} from 'next';
+import { inter } from './ui/fonts';
+
+export const metadata: Metadata = {
+  title: 'ACME'
+}
+
+export default function RootLayout({children}: {children: React.ReactNode}
+
+) {
+  return (
+    <html lang='pt-br'>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
